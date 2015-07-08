327513fccfbde8f2e31aa4e3a8c73578e170d2ae
littlenac <Alex@MacBook-Pro.local>
2015-07-08 14:37:11 -0400

    summeryofthefile

---
+++ page1p.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+
+	<h1>Hello World</h1>
+
+	<p><a href="page1p.php">HOME</a></p>
+	<p><a href="page2.html">CONTACT US</a></p>
+
+	<p>Superfab Office Products - Office Supply Products Division
+
+	<p>The Superfab name has come to mean quality and innovation in advanced office solutions. Never mind mundane office offerings for back-room pencil-pushing geeks ... Superfab offerings are smart, slick, and so far ahead of its time that it's well worth the outrageous prices we demand for them.</p>
+
+	<p>Our award-winning products include: MegalaMartTM, PATTM, Hinternet HunterTM, DAISYTM, and BugSmasher SweetTM, plus other amazing devices that are so secret, if we told you about them we'd then have to swear you to secrecy and degauss your hard drive.</p>
+
+	<div class="img">
+	<img src="superfabceo.jpg" alt="HTML tutorial"</a>
+	</div>
+
+	<p>The biggest corporations all over the world practically flock to our door to have their most ferocious technological challenges turned into purring pussycats by the boy brainiacs and girl geniuses on our staff.</p>
+
+	<p>The Superfab laboratory is located in Sasquatch Falls, Washington (the state whose rich negative ion levels (spawned by high annual rainfall levels) and scenic ocean views seem to inspire technogeeks and propeller-heads to awesome levels of coding productivity ... not that their cubicles actually have a window, or view of any kind).</p>
+
+	<p>The lab is presided over by the legendary Dr. Murry Duchance, PhD, winner of the 1995 Nobel Prize in Physics, 1996 National Science Foundation gold medal, 1997 Thomas Alva Edison Prize, and the 1998 Grammy Award for best new rap album.</p>
+
+	<p>But Dr. Duchance is only one of our many brainiacs.  With so many unmitigated geniuses on the payroll, often stumbling over one another, Superfab likes to get them out of the office from time to time (otherwise they might forget how to tie their shoelaces, and perform other simple social functions that the rest of us take for granted).  Ask us about of in-company supply program!  We'll develop a custom program to your specifications, or simply show up as consultants and dazzle you with our brilliant thinking, and no doubt leave you depressed by your own stupidity.</p>
+	
+	<?php
+	echo date('Y-m-d H:i:s');
+	?>
+
+	</body>
+
+</html>
